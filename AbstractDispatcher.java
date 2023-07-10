ad1c43b4587cab1755c9f7c1563f2938c3324134
wuliiiii <81079286+wuliiiii@users.noreply.github.com>
2023-07-10 23:27:00 +0800

    Add files via upload

--- AbstractDispatcher.java
+++ AbstractDispatcher.java
@@ -1,16 +1,17 @@
-package org.algotithmcontestdatacollect.crawlerdispatcher.Dispatcher;
+package org.algorithmcontestdatacollect.crawlerendpoint2.Dispatcher;
 
 import com.alibaba.fastjson.JSONArray;
+import org.algorithmcontestdatacollect.crawlerendpoint2.Handlers.IHandler;
+import org.algorithmcontestdatacollect.crawlerendpoint2.Spiders.Spider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.stream.RecordId;
-import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.connection.stream.StringRecord;
 import org.springframework.data.redis.serializer.RedisSerializer;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,18 +22,7 @@ public abstract class AbstractDispatcher {
     protected abstract String getHandler();
     protected abstract String getStream();
 
-    public void dispatch(RedisConnectionFactory redisConnectionFactory,JSONArray params){
-        RedisConnection redisConnection = redisConnectionFactory.getConnection();
-        Map<String,String> mp = new HashMap<>();
-        mp.put("spider",getSpider());
-        mp.put("handler",getHandler());
-        mp.put("params",params.toString());
-        StringRecord stringRecord = StringRecord.of(mp).withStreamKey(getStream());
-        RecordId recordId = redisConnection.xAdd(stringRecord.serialize(RedisSerializer.string()));
-        if (recordId!=null){
-            logger.info("成功推送:{}-{},{}",getSpider(),getHandler(),recordId.toString() );
-        }else{
-            logger.error("推送失败:{}-{},{}",getSpider(),getHandler(),params.toString());
-        }
+    public void dispatch(ApplicationContext context,JSONArray params){
+        context.getBean(getHandler(), IHandler.class).handleRequest(context.getBean(getSpider(), Spider.class),params.toString());
     }
 }
