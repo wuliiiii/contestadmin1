20cbd45fa2be3a7141eb4893c234c1b74b0a9bd7
NatalieNataNata <117274414+NatalieNataNata@users.noreply.github.com>
2023-07-17 22:41:16 +0800

    Add files via upload

---
+++ ApplicationComponents/AddCodeforcesAccount.vue
@@ -0,0 +1,95 @@
+<template>
+  <el-dialog v-model="display" @close="close" title="申请收录新codeforces账户" ref="formRef">
+    <el-form :model="data" :rules="rules" ref="formRef" label-width="100px">
+      <el-form-item label="用户名" prop="codeforces_id">
+        <el-input v-model="data.codeforces_id" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="设为主账户" prop="is_main">
+        <el-radio-group v-model="data.is_main">
+          <el-radio label="0">否</el-radio>
+          <el-radio label="1">是</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="close()">取消</el-button>
+        <el-button type="primary" @click="submit" :loading="loading"
+        >提交</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import {reactive, onMounted, ref, defineExpose} from "vue";
+import {ElMessage} from "element-plus";
+import axiosUtil from "../../utils/axiosUtil";
+import simpleUtils from "../../utils/simpleUtils";
+
+let data = ref({
+  codeforces_id: "",
+  is_main: "0",
+})
+let formRef = ref(null)
+let loading = ref(false)
+const display = ref(false)
+let rules = reactive({
+  codeforces_id:[
+    {required: true, message: "请输入用户名", trigger: "blur"},
+    {
+      validator: (rule, value, callback) => {
+        if (simpleUtils.checkCharAndNumberAndUnderscore(value)) {
+          callback()
+        } else {
+          callback(new Error("用户名只能包含数字、字母和下划线"))
+        }
+      }, trigger: "blur"
+    }
+  ],
+  is_main:[
+    {required: true, message: "请选择是否为主账户", trigger: "blur"}
+  ]
+
+})
+let submit = ()=> {
+  if (formRef.value === null) {
+    return
+  }
+  formRef.value.validate((valid) => {
+    if (valid) {
+      loading.value = true
+      axiosUtil.codeforcesAPI.addNewCodeforcesAccount(data.value.codeforces_id,parseInt(data.value.is_main)).then(res=>{
+        if(res.data.code === 200){
+          ElMessage.success("提交成功")
+          close()
+        }else{
+          ElMessage.error(res.data.message)
+        }
+      }).catch(err=>{
+        console.log(err)
+        ElMessage.error("提交失败")
+      }).finally(()=>{
+        loading.value = false
+      })
+    }
+  })
+}
+defineExpose({
+  open() {
+    display.value = true
+  }
+})
+let close = () => {
+  data.value = reactive({
+    codeforces_id: "",
+    is_main: "0",
+  })
+  display.value = false
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ jsonModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-dialog v-model="display" @close="close">
+    <json-viewer
+        :value="jsonData"
+        :expand-depth=5
+        copyable
+        boxed
+        expanded
+        style="text-align: left"
+        sort></json-viewer>
+  </el-dialog>
+</template>
+
+<script setup>
+import {ref, defineProps, defineExpose, onMounted} from "vue";
+const props = defineProps({
+  jsonData: {
+    type: String,
+    default:""
+  },
+})
+const display = ref(false)
+defineExpose({open(){
+  display.value = true
+  }})
+let close = ()=>{
+  display.value = false
+}
+
+
+onMounted(()=>{
+  console.log("mounted")
+})
+
+</script>
+
+<style scoped>
+
+</style>
